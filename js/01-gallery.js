01a29507f505c0c83dc15ed46ae204f2c8b59e5c
Olegmkv <129410354+Olegmkv@users.noreply.github.com>
2023-08-08 21:01:12 +0300

    final1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryImage = document.querySelector(".gallery");
+
+//  створюємо розмітку галереї з preview фото
+const markingGalery = galleryItems.map(({ preview, original, description }) => {return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`}).join("");
+
+// додаємо розмітку галереї в DOM
+galleryImage.insertAdjacentHTML("afterbegin", markingGalery);
+
+// ставимо слухача на всю галерею
+galleryImage.addEventListener('click', onClickGalleryItems);
+
+// обробка кліків на галереї
+function onClickGalleryItems(evt) {
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    showFullImage(evt.target.dataset.source);
+}
+
+// вивід понорозмірного зображення бібліотекою Lightbox
+function showFullImage(image) {
+    const instance = basicLightbox.create(`
+         <img src="${image}" width="800" height="600">
+     `);
+    instance.show();
+}
